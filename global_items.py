cc4feaa57bbb64395665e0face53bf2e30634fe4
Whinit <whinitc2.@gmail.com>
2021-04-16 18:37:15 +0200

    "Discord Compatibility"

--- global_items.py
+++ global_items.py
@@ -1,4 +1,5 @@
 global_items = [
+    ["Kzarka Longbow", 0, 100, 0, 0, 0],
     ["Kzarka Longbow", 1, 100, 0, 0, 0],
     ["Kzarka Longbow", 2, 100, 0, 0, 0],
     ["Kzarka Longbow", 3, 100, 0, 0, 0],
@@ -6,6 +7,11 @@ global_items = [
     ["Kzarka Longbow", 5, 100, 0, 0, 0],
     ["Kzarka Longbow", 6, 100, 0, 0, 0],
     ["Kzarka Longbow", 7, 100, 0, 0, 0],
+    ["Kzarka Longbow", 8, 90, 0, 0, 0],
+    ["Kzarka Longbow", 9, 20.41, 10, 5, 2.5],
+    ["Kzarka Longbow", 10, 14.29, 40, 2.5, 1],
+    ["Kzarka Longbow", 11, 10, 58, 1.8, 0.5],
+    ["Kzarka Longbow", 12, 6.67, 63, 1, 0.15],
     ["Kzarka Longbow", 13, 4, 165, 0.4, 0.08],
     ["Kzarka Longbow", 14, 2.5, 270, 0.25, 0.05],
     ["Kzarka Longbow", 15, 2, 340, 0.2, 0.04],
--- main.py
+++ main.py
@@ -1,23 +1,13 @@
 import random
+import discord
 from global_items import global_items
 from asyncio import sleep
+
+client = discord.Client()
 
 class Item:
     def __init__(self, name: str) -> None:  
         self.name = name
-
-async def grind(s, p):
-    if p.is_grinding is True:
-        return
-
-    p.is_grinding == True
-    money_sec = 200000000 / 3600
-    earned_money = money_sec * s
-    sleep(s)
-    p.inventory.money += round(earned_money)
-    p.is_grinding == False
-    print(p.inventory.money)
-
 
 def to_dict(x):
     return x.__dict__
@@ -62,52 +52,67 @@ class Player:
             price = shop.stock[name]
             if self.inventory.money >= price:
                 self.inventory.money -= price
-                self.inventory.items.append([name, 1])
+                self.inventory.items.append([name, 0])
                 print(self.inventory.items)
             else:
                 print("you dont have enough money")
         else:
             print("item does not exist")
 
-    def enhance(self, name):
-        try:
-            item = list(filter(lambda item: item[0] == name, self.inventory.items))
-            #item[0][1] = item[0][1]
-            if item[0][1] == 20:
-                return
-            search_g = list(filter(lambda global_item: global_item[0] ==  name and global_item[1] == item[0][1], global_items))
-            chance = search_g[0][2]
-            sc = search_g[0][3]
-            pre_sc = search_g[0][4]
-            post_sc = search_g[0][5]
-            fs = self.fs
-            x = random.randint(0, 100)
+    def enhance(self, name, times):
+        for i in range(0, times):
+            try:
+                item = list(filter(lambda item: item[0] == name, self.inventory.items))
+                #item[0][1] = item[0][1]
+                if item[0][1] == 20:
+                    return
+                search_g = list(filter(lambda global_item: global_item[0] ==  name and global_item[1] == item[0][1], global_items))
+                chance = search_g[0][2]
+                sc = search_g[0][3]
+                pre_sc = search_g[0][4]
+                post_sc = search_g[0][5]
+                fs = self.fs
+                print("fs: " + str(fs))
+                x = random.randint(0, 100)
 
-            if fs < sc:
-                chance += fs*pre_sc
-            elif fs >= post_sc:
-                chance += sc*pre_sc
-                chance += (fs - sc)*post_sc
-            
-            if chance > 90:
-                chance = 90
-            
-            if item[0][1] < 8:
-                chance = 100
+                if fs < sc:
+                    chance += fs*pre_sc
+                elif fs >= post_sc:
+                    chance += sc*pre_sc
+                    chance += (fs - sc)*post_sc
+                
+                if chance > 90:
+                    chance = 90
+                
+                if item[0][1] <= 7:
+                    chance = 100
+                elif item[0][1] == 8:
+                    chance = 90
 
-            if x <= chance:
-                if item[0][1] > 8:
-                    self.fs = 0
-                item[0][1] += 1
-                print("enchancment succeded")
-                print(item[0][1])
-            else:
-                print("enhancment failed")
-                self.fs += 1
-                if item[0][1] > 16:
-                    item[0][1] -= 1
-        except:
-            print("Item is not in the inventory")
+                if x <= chance:
+                    if item[0][1] > 8:
+                        self.fs = 0
+                    print("enchancment succeded " + str(item[0][0]) + " +" + str(item[0][1]))
+                    item[0][1] += 1
+                else:
+                    print("enhancment failed " + str(item[0][0]) + " +" + str(item[0][1]))
+                    self.fs += 1
+                    if item[0][1] > 16:
+                        item[0][1] -= 1
+            except:
+                print("Item is not in the inventory")
+        
+    async def grind(self, s):
+        if self.is_grinding is True:
+            return
+        self.is_grinding = True
+        money_sec = 200000000 / 3600
+        earned_money = money_sec * s
+        await sleep(s)
+        self.inventory.money += round(earned_money)
+        self.is_grinding == False
+        print(self.inventory.money)
+
 
 class Game:
     def __init__(self, players: list = None) -> None:
@@ -116,20 +121,26 @@ class Game:
         else:
             self.players = players
 
-p = Player(0, "Terry", 9999)
+p = Player(0, "Terry", 10)
 s = Shop()
-s.add_item(to_dict(Item("Rusty Sword")), 20)
-s.add_item(to_dict(Item("Rusty Axe")), 10)
 s.add_item(to_dict(Item("Kzarka Longbow")), 20)
+@client.event
+async def on_message(message):
+    global s
+    if message.author == client.user:
+        return
 
-#p.buy_item("Rusty Sword", s)
-p.buy_item("Kzarka Longbow", s)
-#p.buy_item("Rusty Axe", s)
-#p.buy_item("Rusty Axe", s)
+    if message.content.startswith('$grind'):
+        sec = message.content[7:]
+        await p.grind(int(sec))
+    
+    if message.content.startswith("$buy"):
+        item = message.content[5:]
+        p.buy_item(item, s)
+    
+    if message.content.startswith("$enhance"):
+        item = message.content[9:]
+        p.enhance(item, 1)
 
-#p.enhance("Rusty Axe")
-#p.enhance("Rusty Axe")
-#p.enhance("Rusty Sword")
-p.enhance("Kzarka Longbow")
-grind(2, p)
-p.buy_item("Kzarka Longbow", s)
+
+client.run("ODMyNTgyODczNzA1ODczNDI5.YHl5OQ.ndiEdf5QOgqKrtXgLRmeX6bFo8w")
